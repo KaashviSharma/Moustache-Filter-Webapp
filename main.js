4776510025a455c2d55095539013354fd9694b9d
KaashviSharma <87973727+KaashviSharma@users.noreply.github.com>
2022-04-25 20:27:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
+x = 0;
+y = 0;
+
 function preload()
 {
-
+   moustache = loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
 }
 
 function setup()
 {
    canvas = createCanvas(300,300);
-   canvas.position(600,300);
+   canvas.position(650,300);
    video = createCapture(VIDEO);
    video.size(300,300);
    video.hide();
@@ -19,20 +22,23 @@ function modelLoaded()
    console.log("poseNet is initialized");
 }
 
-function draw()
-{
-   image(video,0,0,300,300);
-}
-
 function gotPoses(results)
 {
    if(results.length > 0)
    {
       console.log(results);
+      x = results[0].pose.nose.x-30;
+      y  = results[0].pose.nose.y+10;
       console.log("nose x ="+results[0].pose.nose.x);
       console.log("nose y ="+results[0].pose.nose.y);
 
    }
+}
+
+function draw()
+{
+   image(video,0,0,300,300);
+   image(moustache,x,y,60,30);
 }
 
 function take_snapshot()
